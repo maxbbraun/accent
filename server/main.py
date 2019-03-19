d6d75273d530a923c4b6ad21872609022893f27d
Max Braun <max.braun@gmail.com>
2019-03-18 22:02:20 -0700

    Add debug endpoints for each image type

--- server/main.py
+++ server/main.py
@@ -13,9 +13,11 @@ from numpy import sum
 from PIL import Image
 from time import time
 
+from artwork import get_artwork_image
+from commute import get_commute_image
+from g_calendar import get_calendar_image
 from schedule import get_scheduled_delay
 from schedule import get_scheduled_image
-
 
 # The dimensions of the display.
 DISPLAY_WIDTH = 640
@@ -37,49 +39,73 @@ def _vq(obs, code_book):
     obs_repeated = obs.repeat(len(code_book), axis=0).reshape(repeated_shape)
     differences = obs_repeated.astype("float") - code_book
     distances = sum(differences ** 2, axis=2)
-    indices = argmin(distances, axis=1)
-    return indices
+    return argmin(distances, axis=1)
 
 
-def _get_image():
-    """Generates the image to show."""
+def _get_indices(image):
+    """Maps each pixel of an image to 0 (black), 1 (white), or 2 (red)."""
 
-    # Get the scheduled image.
-    image = get_scheduled_image(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    image_data = array(image).reshape((DISPLAY_WIDTH * DISPLAY_HEIGHT, 3))
+    return _vq(image_data, BWR_8_BIT)
+
+
+def _send_png(image):
+    """Creates a PNG response from the specified image."""
 
     # Map each color to the closest black, white, or red.
-    image_data = array(image).reshape((DISPLAY_WIDTH * DISPLAY_HEIGHT, 3))
-    indices = _vq(image_data, BWR_8_BIT)
+    indices = _get_indices(image)
     bwr_image_data = BWR_8_BIT[indices.reshape(
         (DISPLAY_HEIGHT, DISPLAY_WIDTH))]
     bwr_image = Image.fromarray(bwr_image_data)
 
-    return bwr_image
-
-
-@app.route("/png")
-def png():
-    """Responds with a PNG image for debugging."""
-
-    image = _get_image()
-
     # Encode the image to PNG bytes.
     buffer = BytesIO()
-    image.save(buffer, format="png")
+    bwr_image.save(buffer, format="png")
     buffer.seek(0)
 
     return send_file(buffer, mimetype="image/png", cache_timeout=0)
 
 
+@app.route("/artwork")
+def artwork():
+    """Responds with a PNG version of the artwork image."""
+
+    image = get_artwork_image(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    return _send_png(image)
+
+
+@app.route("/commute")
+def commute():
+    """Responds with a PNG version of the commute image."""
+
+    image = get_commute_image(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    return _send_png(image)
+
+
+@app.route("/calendar")
+def calendar():
+    """Responds with a PNG version of the calendar image."""
+
+    image = get_calendar_image(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    return _send_png(image)
+
+
+@app.route("/png")
+def png():
+    """Responds with a PNG version of the scheduled image."""
+
+    image = get_scheduled_image(DISPLAY_WIDTH, DISPLAY_HEIGHT)
+    return _send_png(image)
+
+
 @app.route("/epd")
 def epd():
-    """Responds with image data encoded for the EPD."""
+    """Responds with an e-paper display version of the scheduled image."""
 
-    image = _get_image()
+    image = get_scheduled_image(DISPLAY_WIDTH, DISPLAY_HEIGHT)
 
     # Encode the image to 2-bit black, white, or red.
-    image_data = array(image).reshape((DISPLAY_WIDTH * DISPLAY_HEIGHT, 3))
-    indices = _vq(image_data, BWR_8_BIT)
+    indices = _get_indices(image)
     bwr_image_data = BWR_2_BIT[indices.reshape(
         (DISPLAY_HEIGHT * DISPLAY_WIDTH))]
     bwr_bytes = packbits(bwr_image_data).tostring()
@@ -93,7 +119,6 @@ def next():
     """Responds with the milliseconds until the next image."""
 
     milliseconds = get_scheduled_delay()
-
     return Response(str(milliseconds), mimetype="text/plain")
 
 
